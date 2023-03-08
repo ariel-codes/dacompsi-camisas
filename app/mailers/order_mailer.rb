6d729ac473e3a6317b668b18465c3c840cb5e1ce
Ariel Santos <ariel.santos@dcc.ufmg.br>
2023-03-08 12:07:35 -0300

    Unify button styles, fix mailers

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -9,18 +9,18 @@ class OrderMailer < ApplicationMailer
     @order = params[:order]
     @order_items = @order.cart_products
 
-    mail to: @buyer.email, subject: "[DACompSI] Confirmação de Compra - Pedido ##{order.id}}"
+    mail to: @buyer.email, subject: "[DACompSI] Confirmação de Compra - Pedido ##{@order.id}}"
   end
 
   def paid
     @order = params[:order]
 
-    mail to: @order.buyer.email, subject: "[DACompSI] Confirmação de Pagamento - Pedido ##{order.id}}"
+    mail to: @order.buyer.email, subject: "[DACompSI] Confirmação de Pagamento - Pedido ##{@order.id}}"
   end
 
   def error
     @order = params[:order]
 
-    mail to: @order.buyer.email, subject: "[DACompSI] Erro no Pagamento - Pedido ##{order.id}}"
+    mail to: @order.buyer.email, subject: "[DACompSI] Erro no Pagamento - Pedido ##{@order.id}}"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@ camisa23_1 = Product.create! price: 32_00, name: "Camisa de Curso",
   thumb_path: "camisa-thumb.png",
   carousel_paths: %w[camisa-cc.png camisa-si.png camisa-tamanhos.png],
   variations: {
+    "corte" => %w[t-shirt babylook],
     "curso" => %w[Computação Sistemas],
-    "tamanho" => %w[P PP M G GG XG],
     "cor" => %w[preto branco azul vinho cinza rosa petróleo verde],
-    "corte" => %w[t-shirt babylook]
+    "tamanho" => %w[PP P M G GG XG]
   }
 
 jaqueta23_1 = Product.create! price: 172_00, name: "Jaqueta DCC",
   thumb_path: "jaqueta-thumb.png",
   carousel_paths: %w[jaqueta-frente.png jaqueta-verso.png jaqueta-tamanhos.png],
-  variations: {"tamanho" => %w[P PP M G GG]}
+  variations: {"tamanho" => %w[PP P M G GG]}
 
 campanha23_1 = Campaign.create! name: "Camisas de Curso 2023/1",
   products: [camisa23_1, jaqueta23_1],
