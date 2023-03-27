5d488a65a6f287447c20811112f718a4f6d8c652
Ariel Santos <ariel.santos@dcc.ufmg.br>
2023-03-27 00:07:25 -0300

    Make Campaign::active respect timezone

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,7 @@
 class Campaign < ApplicationRecord
   has_and_belongs_to_many :products
 
-  scope :active, -> { where("start <= :today AND end >= :today", {today: Date.today}) }
+  scope :active, -> { where("start <= :today AND end >= :today", {today: Time.zone.today}) }
 
   validates :name, :start, :end, presence: true
 end
